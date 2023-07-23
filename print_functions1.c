23936d8a740bfd643783a7c2c72002fd96553b95
kellyinfortech <109739583+kellyinfortech@users.noreply.github.com>
2023-07-23 21:14:04 +0300

    Update print_functions1.c

--- print_functions1.c
+++ print_functions1.c
@@ -7,7 +7,7 @@
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width
- * @precision: Precision specification
+ * @precision: Precision specifier
  * @size: Size specifier
  * Return: Number of chars printed.
  */
