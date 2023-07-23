512a3eea30cd3da42dbde7a90062cd6606b298cf
kellyinfortech <109739583+kellyinfortech@users.noreply.github.com>
2023-07-23 21:05:03 +0300

    Update get_width.c

--- get_width.c
+++ get_width.c
@@ -2,7 +2,7 @@
 
 /**
  * get_width - Calculates the width for printing
- * @format: Formatted string in which to print the arguments.
+ * @format: Formatted string to print the arguments an d pointer.
  * @i: List of arguments to be printed.
  * @list: list of arguments.
  * Return: width.
